01e7417150d6a6ff4659525ebf8ff73eefa96adc
yash mishra <46112945+yash547@users.noreply.github.com>
2024-06-27 22:10:31 +0530

    Update RegisServiceInterface.java

--- src/main/java/com/govpoly/chardhamapis/ServiceInterface/RegisServiceInterface.java
+++ src/main/java/com/govpoly/chardhamapis/ServiceInterface/RegisServiceInterface.java
@@ -5,8 +5,16 @@ import com.govpoly.chardhamapis.Entity.RegistrationEntity;
  * 
  * 
  * @author Team CharDham Yatra Developer
+ * Here we have create the RegistrationEntity interface as the name of 
+ * RegisServiceInterface.
  */
 public interface RegisServiceInterface {
+
+    /**
+      here the below method are declare.
+      and they are inherite into the RegistrationServiceImple class for there 
+      business logic.
+    **/
     
     public Object createAccount(RegistrationEntity rEntity);
 
@@ -15,7 +23,6 @@ public interface RegisServiceInterface {
     public Object forgotPasswordByEmailId(String emailId);
     
     public Object passwordUpdtByEmailId(String emailId,String password);
-
 
     public Object updateProfile(RegistrationEntity registrationEntity);
     
